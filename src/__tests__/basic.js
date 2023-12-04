abc456f219df042f60a712810c114bbedddfea9a
Sliva Ivan <ivan.sliva2017@yandex.ru>
2023-12-04 22:49:15 +0300

    initial commit

--- src/__tests__/basic.js
+++ src/__tests__/basic.js
@@ -1,13 +1,13 @@
 import sortHeroes, { heroes } from '../index';
 
 test('checking units health 100', () => {
-  expect(sortHeroes(heroes)).toEqual({ name: 'маг', health: 100 });
+  expect(sortHeroes(heroes)[0]).toEqual({ name: 'маг', health: 100 });
 });
 
 test('checking units health 80', () => {
-  expect(sortHeroes(heroes)).toEqual({ name: 'маг', health: 80 });
+  expect(sortHeroes(heroes)[1]).toEqual({ name: 'лучник', health: 80 });
 });
 
 test('checking units health 10', () => {
-  expect(sortHeroes(heroes)).toEqual({ name: 'маг', health: 10 });
+  expect(sortHeroes(heroes)[2]).toEqual({ name: 'мечник', health: 10 });
 });
